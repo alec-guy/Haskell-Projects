71a91ab4d144a31ff76efd556169ebd31234df17
alec <alecrodriguez1247@gmail.com>
2024-12-27 18:03:49 -0600

    trying to find an api

--- Haskell/weatherCLI/app/Main.hs
+++ Haskell/weatherCLI/app/Main.hs
@@ -14,13 +14,6 @@ import Control.Exception (catch, SomeException(..))
 import Data.Aeson
 
 
-myapikey :: String 
-myapikey = "ce9af72a7377c63adb3912182c6711ed"
-
-insertZipGeo :: Zipcode -> String 
-insertZipGeo (Zipcode zipcode) = 
-    "/geo/1.0/zip?zip=" ++ zipcode ++ ",US&appid=" ++ myapikey 
-
 getZipCode :: Command -> Zipcode 
 getZipCode command = 
     case command of 
@@ -29,6 +22,9 @@ getZipCode command =
         (Humidity zipcode)        -> zipcode 
         (Temp zipcode)            -> zipcode 
         (Time zipcode)            -> zipcode 
+
+hostnameAPI :: Request 
+hostnameAPI = undefined 
 
 main :: IO ()
 main = do 
@@ -39,18 +35,10 @@ main = do
     case parse parseCommand "" command of 
         Left err  -> Prelude.putStrLn $ errorBundlePretty err
         Right com -> do 
-            latloncityR  <-  let bs = httpBS (setRequestPath (C8.pack $ insertZipGeo $ getZipCode com) "http://api.openweathermap.org") 
-                             in  bs 
-            let maybelatloncity = (decode $ LBS.fromStrict $ getResponseBody latloncityR)  :: (Maybe LatLonCity)
-            case maybelatloncity of 
-                Nothing -> do 
-                    Prelude.putStrLn  "Could not find out your city, nor geolocation necessary to get weather."
-                    Prelude.putStrLn  "I am sorry I cannot offer a better error message."
-                    Prelude.putStrLn  "Maybe its the API Key, try changing it."
-                    Prelude.putStrLn  "Restarting program."
-                    main 
-                Just latloncity -> do 
-                   Prelude.putStrLn $ show latloncity
+            bodyR <- let bs = httpBS hostnameAPI 
+                     in  bs
+            return ()
+
 
             
 
--- Haskell/weatherCLI/lib/Types.hs
+++ Haskell/weatherCLI/lib/Types.hs
@@ -13,14 +13,4 @@ data Command = TodayWeather Zipcode
 
 newtype Zipcode = Zipcode String  deriving (Show, Eq)
 
--- {"zip":"78521","name":"Brownsville","lat":25.9221,"lon":-97.4612,"country":"US"}
 
-data LatLonCity = LatLonCity 
-                { zip     :: String 
-                , name    :: String 
-                , lat     :: Float 
-                , lon     :: Float
-                , country :: String 
-                }
-                deriving (Show, Eq, Generic)
-instance FromJSON LatLonCity where 
