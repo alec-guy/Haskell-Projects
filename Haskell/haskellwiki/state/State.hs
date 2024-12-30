83de6e6705347e1716a0aae12422cff79dcbaf67
alec <alecrodriguez1247@gmail.com>
2024-12-30 13:33:10 -0600

    cringe

--- Haskell/haskellwiki/state/State.hs
+++ Haskell/haskellwiki/state/State.hs
@@ -94,8 +94,15 @@ compose f g = \s0 -> let (a1, s1) = f s0 in (g a1) s1
 {-This lambda expression threads both intermediate results produced by f into those required by g -}
 
 coinS , pushS :: State TurnstileState TurnstileOutput 
-coinS = State coin 
-pushS = State push
+coinS = do 
+    unit <- put Unlocked 
+    return $ Thank 
+pushS = do 
+    s <- get 
+    put Locked 
+    case s of 
+        Locked   -> return Tut 
+        Unlocked -> return Open 
 
 mondayS :: State TurnstileState [TurnstileOutput]
 mondayS = do 
@@ -105,3 +112,49 @@ mondayS = do
     a4 <- coinS 
     a5 <- pushS 
     return [a1,a2,a3,a4,a5]
+
+regularPersonS, hastyPersonS, distractedPersonS :: State TurnstileState [TurnstileOutput]
+regularPersonS = sequence [coinS , pushS]
+hastyPersonS = do 
+    a1 <- pushS 
+    case a1 of 
+        Open -> do 
+                 a2 <- regularPersonS 
+                 return $ Tut : a2 
+        Tut  -> return [Open] 
+distractedPersonS = sequence [coinS]
+
+evalState :: State s a -> s -> a 
+evalState sf s = fst (runState sf s)
+
+execState :: State s a -> s -> s 
+execState sf s = snd (runState sf s)
+
+testTurnStile :: State TurnstileState Bool 
+testTurnStile = do 
+    --somehow set state to locked 
+    put Locked 
+    check1 <- pushS 
+    --somehow set state to Unlocked 
+    put Unlocked 
+    check2 <- pushS 
+    --somehow set state to locked again 
+    _        <- coinS 
+    check3   <- get 
+    put Locked
+    return (check1 == Tut && check2 == Open && check3 == Unlocked)
+
+put :: s -> State s ()
+put newState = state $ \_ -> ((), newState)
+
+get :: State s s
+get = state $ \s -> (s, s)
+
+modify :: (s -> s) -> State s ()
+modify f = do 
+    s <- get 
+    put (f s)
+gets :: (s -> a) -> State s a 
+gets f = do 
+    s <- get 
+    return (f s)
