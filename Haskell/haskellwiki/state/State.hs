60ccd41babd6ed7eff3df85e3e04455bbbbccb2e
alec <alecrodriguez1247@gmail.com>
2024-12-30 09:25:29 -0600

    tuesday

--- Haskell/haskellwiki/state/State.hs
+++ Haskell/haskellwiki/state/State.hs
@@ -25,9 +25,29 @@ monday s0 =
       (a5, s5) = push s4 
   in   ([a1,a2,a3,a4,a5], s5)
 
-regularPerson :: TurnstileState -> ([TurnstileOutput], TurnstileState)
+regularPerson, distractedPerson, hastyPerson :: TurnstileState -> ([TurnstileOutput], TurnstileState)
+
 regularPerson s0 = 
     let (a1, s1) = coin s0 
         (a2,s2)  = push s1
     in ([a1,a2], s2)
 
+distractedPerson s0 = 
+    let (a1,s1) = coin s0 
+    in (,) [a1] s1
+
+hastyPerson s0 = 
+    let (a1,s1) = push s0 
+    in 
+    case a1 of 
+        Tut    -> ([a1], s1) 
+        Open   -> let (outputs, st) = regularPerson s1 
+                 in (a1 : outputs, st)
+
+tuesday :: TurnstileState -> ([TurnstileOutput], TurnstileState)
+tuesday s0 = do 
+     (st)   <- regularPerson s0
+     (st') <- hastyPerson st 
+     (st2) <- distractedPerson st' 
+     (st3) <- hastyPerson st2 
+     return st3
